06583510c044881bad49cdeb7b625eb3e928ad5d
mehdi parastar <parastar.ehdi@gmail.com>
2025-01-11 21:33:23 +0330

    optimising Dockerfiles for deploying

--- apps/invoice/test/app.e2e-spec.ts
+++ apps/invoice/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/invoce.module';
+import { InvoiceModule } from '../src/invoice.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [InvoiceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
